fc0b509b3a4b467954c48b14cfcfc36811604898
Oleksandr <bajmutov@gmail.com>
2023-10-26 17:20:50 +0300

    allowSyntheticDefaultImports

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,11 @@
-// import { concatenation } from './concatenation';
+import { concatenation } from './concatenation';
 function add(num1, num2) {
     return num1 + num2;
 }
 console.log('sum', add(1, 1));
+//  -----------------
 const button = document.querySelector('button');
 const input = document.querySelector('input');
-function concatenation(firstWord, secondWord) {
-    console.log(`${firstWord} ${secondWord}`);
-}
 if (button && input) {
     button.addEventListener('click', () => {
         concatenation(input.value, 'Hello world. Peace everyone!!');
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-// import { concatenation } from './concatenation';
+import { concatenation } from './concatenation';
 
 function add(num1: number, num2: number) {
   return num1 + num2;
 }
 console.log('sum', add(1, 1));
 
+//  -----------------
+
 const button = document.querySelector('button')!;
 const input = document.querySelector('input')!;
-
-function concatenation(firstWord: string, secondWord: string) {
-  console.log(`${firstWord} ${secondWord}`);
-}
 
 if (button && input) {
   button.addEventListener('click', () => {
